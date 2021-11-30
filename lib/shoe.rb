5cf3b683a53c9abea1018dfe5fd8a13763623092
Joshua <wilson.joshua@outlook.com>
2021-11-30 12:32:47 -0800

    commit tree 73aea14380eade0056b91a71a8cbdb385759410d

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
-# Make your shoe class here!
+#  need class of shoe, has a brand, color, size, material, condition
+# need cobble function, turn old shoe into new shoe
+
+class Shoe
+attr_accessor :color, :size, :material, :condition
+attr_reader :brand
+
+def initialize(brand)
+    @brand = brand
+end
+
+def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+end
+
+end
